e3e5c4f61c768b7b7b500a477deb9438ed26fe87
Muhammed Zabin M <zabinmuhammed0000@gmail.com>
2026-03-19 16:45:30 +0530

    redis implemented

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const app = express()
 const DB = require('./DB/config')
 const router = require('./Router/router')
 const http = require('http')
 const initSocket = require('./socket')
-app.use(cors())
-app.use(express.json())
-app.use(('/api'),router)
+const { connectRedis, redisClient, testRedis } = require('./redis')
+const app = express()
+const startServer = async () => {
+    await connectRedis()  
+    await testRedis()   
+    
+    app.use(cors())
+    app.use(express.json())
+    app.use(('/api'),router)
 
-const server = http.createServer(app)
-initSocket(server)
+    const server = http.createServer(app)
+    initSocket(server)
 
-const PORT = process.env.PORT || 8000
-server.listen(PORT,()=>{
-    console.log('server is running on PORT', PORT);
-})
+    const PORT = process.env.PORT || 8000
+    server.listen(PORT,()=>{
+        console.log('server is running on PORT', PORT);
+    })
+}
+startServer()
---
+++ Backend/redis.js
@@ -0,0 +1,23 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+    url:'redis://127.0.0.1:6379' //tell node redis->local machine address->redis port
+})
+
+redisClient.on("error",(err)=>{
+    console.log("Redis Error", err);
+})
+
+const connectRedis = async()=>{
+    await redisClient.connect()
+    console.log('Redis Connected')
+}
+
+const testRedis = async()=>{
+   await redisClient.set("test","deyyyyyyyyyy")
+    const data = await redisClient.get('test')
+    console.log("Redis Test:" ,data);
+}
+
+module.exports = { redisClient,connectRedis,testRedis }
+ //use in routes and handlers to send data to redis,connect redis woth our app
